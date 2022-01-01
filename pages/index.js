b872e1bff84270f1f16f72a86d77e5d4e10963c9
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2021-12-31 23:07:09 -0500

    Removing Unused Imports.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from "react";
 import axios from "axios";
 import { FaCaretUp, FaCaretDown } from "react-icons/fa";
 import { Line } from "react-chartjs-2";
